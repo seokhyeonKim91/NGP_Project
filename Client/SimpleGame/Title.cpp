6b1bff536d3060c2800df913519eeb7ea9b1c654
seokhyeonKim91 <tjrgus7127@naver.com>
2022-12-08 12:57:00 +0900

    타이틀 컬러 변경

--- Client/SimpleGame/Title.cpp
+++ Client/SimpleGame/Title.cpp
@@ -13,5 +13,5 @@ Title::~Title()
 void Title::RendererScene()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 }
